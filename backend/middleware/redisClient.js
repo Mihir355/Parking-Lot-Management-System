eb0f23610ca55347ba51a866053aae3c483fb0ab
Mihir Biswas <biswasmihir355@gmail.com>
2025-05-04 19:30:21 +0530

    modified redis client

--- backend/middleware/redisClient.js
+++ backend/middleware/redisClient.js
@@ -1,7 +1,13 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  url: "rediss://default:9Zlg69KsL4IVlVCiRl44nEoduwsKgYnZ@redis-18782.c305.ap-south-1-1.ec2.redns.redis-cloud.com:18782",
+  socket: {
+    host: "redis-18782.c305.ap-south-1-1.ec2.redns.redis-cloud.com", // e.g. redis-xxxx.c10.us-east-1-4.ec2.cloud.redislabs.com
+    port: 18782, // e.g. 12345
+    tls: true, // ✅ enable SSL/TLS
+  },
+  username: "default",
+  password: "9Zlg69KsL4IVlVCiRl44nEoduwsKgYnZ",
 });
 
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
