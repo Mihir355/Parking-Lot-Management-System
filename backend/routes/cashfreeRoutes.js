e6d42292d6dd596b2380e0c5fa21853a71be7092
Mihir Biswas <biswasmihir355@gmail.com>
2025-06-12 18:48:56 +0530

    redirect url change

--- backend/routes/cashfreeRoutes.js
+++ backend/routes/cashfreeRoutes.js
@@ -28,7 +28,7 @@ router.post("/create-order", async (req, res) => {
           customer_phone: customerPhone,
         },
         order_meta: {
-          return_url: `https://yourdomain.com/home?order_id=${orderId}&status={order_status}`,
+          return_url: `https://parking-lot-management-system-1.onrender.com/home?order_id=${orderId}&status={order_status}`,
         },
       },
       {
